a0e1a631b6f536e0cec0250d54bb6a59d4f1493b
Nana <rupangshudutta@gmail.com>
2022-10-14 01:27:04 +0530

    java basic what I learn

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,7 +1,129 @@
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Main {
+    /*static int sum(int a,  int b){
+        return a+b;
+    }
+     */
+
     public static void main(String[] args) {
-        //System.out.println("Hello world!");
+       /* System.out.println("Hello world!");
         String name ="Nana";
         System.out.println(name);
+        int a=5;
+        float b =-12.3f;
+        boolean raja = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(raja);
+        byte c =18;
+        double d =12.564758d;
+        char e ='A';
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        */
+        /* int num1=10 , num2= 5;
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1/num2);
+        System.out.println(num1*num2);
+        System.out.println(num1--);
+        System.out.println(--num1);
+        System.out.println(num1++);
+        System.out.println(++num1);
+         */
+        /*Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name >>");
+        String input = scan.nextLine();
+        System.out.println(input);
+
+        Scanner scan1 = new Scanner(System.in);
+        System.out.println("Enter your age >>");
+        int input1 = scan1.nextInt();
+        System.out.println(input1);
+         */
+        /*String name = "Nana";
+        System.out.println(name.length());
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.contains("a"));
+        System.out.println(name.charAt(2));
+        System.out.println(name.endsWith("na"));
+        System.out.println(name.indexOf("na"));
+         */
+        /*int num3=3 , num4=10;
+        System.out.println(Math.max(num3,num4));
+        System.out.println(Math.min(num3,num4));
+        System.out.println(Math.abs(-4));
+        System.out.println(Math.sqrt(41));
+        System.out.println(Math.random());
+        System.out.println(4+(8-4)*Math.random());//to get random no 8-4;// for mor information google :java math class method
+         */
+       /* Scanner scan2 = new Scanner(System.in);
+        System.out.println("enter Your age ");
+        int age = scan2.nextInt();
+        if (age < 18){
+            System.out.println("O kid You are underage for this ride !");
+        }
+        else if (age>=19 && age<=40) {
+            System.out.println("Go enjoy your ride");
+        }
+        else {
+            System.out.println("Sorry! you are over age for this ride");
+        }
+
+        switch (age){
+            case 19:
+                System.out.println("your age is 19");
+                break;
+            case 40:
+                System.out.println("your age is 40");
+                break;
+            default:
+                System.out.println("Your age in not in the list");
+                break;
+        }
+        */
+       /* int f =10;
+        while (f<12){
+            System.out.println(f);
+            f++;
+        }
+        do {
+            System.out.println(f);
+            f++;
+        }while (f<20);
+
+        for (int g=0;g<19;g++){
+            if (g==4){
+                continue;//to skip 4 ;
+            }
+            System.out.println(g);
+        }
+        */
+        /*int [] marks={1,2,3,4,5,6};
+        marks[2]=23;
+        System.out.println(marks[2]);
+        for (int value:marks) {
+            System.out.println(value);
+        }
+        int [][] matrix ={{1,2,3},{4,5,6}};
+        System.out.println(matrix[0][2]);
+        int [] try_catch={1,2,3,4,5,6};
+        try {
+            System.out.println(try_catch[8]);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+        System.out.println("masoom");
+         */
+        /*//recall from static int sum
+        System.out.println(sum(1,3));
+         */
+
+
     }
 }
