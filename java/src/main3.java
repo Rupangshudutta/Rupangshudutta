94030e56ba137ccb2ee5d2cee777915ea8381c43
Nana <rupangshudutta@gmail.com>
2022-10-19 20:03:01 +0530

    This is all from sradhya didi what I larn java basics

---
+++ java/src/main3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class main3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("================================");
+        //for(int i=0;i<3;i++){
+            String s =sc.nextLine();
+            //sc.nextLine();
+            int x=sc1.nextInt();
+           // if(i==3){
+                System.out.println(s);
+           // }
+            //Complete this line
+        //}
+        sc.close();
+        sc1.close();
+        System.out.println("================================");
+
+    }
+}
---
+++ java/src/main_4_aman.java
@@ -0,0 +1,109 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class main_4_aman {
+    public static void main(String[] args) {
+       /* //System.out.println("Hello world !");
+        //byte - 1 [-128 to 127]
+        String a = "apu";
+        String b = new String("akku");//this is not important
+        System.out.println(a.length());
+        System.out.println(a.charAt(0));
+        String c = a.replace('a', 'b');
+        System.out.println(c);
+        String name = "Nana and pankaj ";
+        System.out.println(name.substring(0,5));
+        int [] marks = new int[4];
+        marks[0]= 12;
+        marks [1]= 44;
+        marks [2] = 23;
+        marks [3] = 63;
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        //System.out.println(marks);//not print the arrey marks print another something.
+        //length
+        System.out.println(marks.length);
+
+        //sort
+        Arrays.sort(marks);
+        System.out.println(marks[2]);
+        int [] marks2 = {23,43,45};
+        int [] [] final_marks = {{12,34,56},{23,45,67}};
+        System.out.println(final_marks[1][2]);
+        //casting
+        double p = 100.00;
+        double fp = p + 18.00;
+        System.out.println(fp);
+
+        int price =100;
+        int final_price = price +(int)18.00;
+        System.out.println(final_price);
+
+        //final value (constsant)
+        final float PI =3.14F;
+       // PI = 1.1F; // if I creat final value in one things then it shows a red line because it says you can't chenge the value from final.
+
+        System.out.println(Math.max(45,56));
+        System.out.println(Math.min(23,34));
+        System.out.println((int)(Math.random()*100));//random
+        Scanner Scan = new Scanner(System.in);
+        int d = Scan.nextInt();
+        System.out.println(d);
+        //comparison Operator
+        // do while loop
+        Scanner scan1 = new Scanner(System.in);
+        int numb = 0;
+
+        do {
+            System.out.println("Enter a no >>");
+            numb = scan1.nextInt();
+            System.out.print("Your no is :");
+            System.out.println(numb);
+        }while (numb>=0);
+        System.out.println("The End");
+
+
+        int i =0;
+        while (true){
+            System.out.println(i);
+            i++;
+            if(i==3){
+                i++;
+                continue;
+            }
+            if(i==5){
+                break;
+            }
+        }
+        //try catch;
+        //methods ., funtion calling by method.
+
+        */
+
+        /*
+        // mini project
+        int a = ((int)(Math.random()*100));
+        for(int i =0;i<=a;i++){
+            System.out.println("Enter A no between 1-100 >>");
+            Scanner scan = new Scanner(System.in);
+            int b = scan.nextInt();
+
+            if (b<0){
+                break;
+            }
+            if (b == a) {
+                System.out.println("Congratulation! You guess it in " + i + " Attempted");
+                break;
+            }
+            else if (b<a) {
+                System.out.println("You entered a small no , Try with greater one");
+            }
+            else {
+                System.out.println("You entered a big no , Try with Smaller one ");
+            }
+        }
+
+        */
+
+    }
+}
